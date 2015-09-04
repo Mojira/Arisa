21b97a47c27781aa759daa3953f850d10d5acb3c
Synchunk <synchunk@web.de>
2015-09-04 17:08:51 +0300

    Add label Stop Words

--- config/labels.rb
+++ config/labels.rb
@@ -6,7 +6,15 @@ VALID_LABEL_REGEXP = %r{\A[0-9a-z/][a-z-]{,30}[0-9a-z]\z}i
 # Labels will be converted to lower case before they're checked against this.
 LABEL_BLACKLIST = %w(
   bad bug bugs ce error game help ios ipad iphone ipod mcce mcpe pc pe play
-  scrolls
+  scrolls about above after again against all am an and any are as at be because
+  been before being below between both but by cannot could did do does doing
+  down during each few for from further had has have having he her here hers
+  herself him himself his how if in into is it its itself me more most my
+  myself no nor not of off on once only or other ought our ours ourselves out
+  over own same she should so some such than that the their theirs them
+  themselves then there these they this those through to too under until up
+  very was we were what when where which while who whom why with would you your
+  yours yourself yourselves
 )
 
 # A list of regular expressions that match labels which should be removed.
