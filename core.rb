5c0b4b77d20527a0dfd29bb3b89605b3bfcc6d87
Ezekiel Elin <ezekielelin@me.com>
2015-09-08 15:43:27 -0400

    Indenting, take 2

--- core.rb
+++ core.rb
@@ -9,30 +9,30 @@ Dir[files].each { |file| require file }
 
 # Arisa project module
 module Arisa
-	# Stores the JIRA client and starts the main loop
-	class Core
-		attr_reader :client, :dispatcher, :versions
-		@arguments = Arguments.new(ARGV)
+    # Stores the JIRA client and starts the main loop
+    class Core
+        attr_reader :client, :dispatcher, :versions
+        @arguments = Arguments.new(ARGV)
 
-		def initialize
-			@client = JIRA::Client.new(CLIENT_OPTIONS)
-			@dispatcher = Dispatcher.new(self)
-			@versions = Versions.new(@client)
-		end
+        def initialize
+            @client = JIRA::Client.new(CLIENT_OPTIONS)
+            @dispatcher = Dispatcher.new(self)
+            @versions = Versions.new(@client)
+        end
 
-		def start
-			loop do
-				@dispatcher.dispatch
-				sleep @dispatcher.task_delay
-			end
-		end
+        def start
+            loop do
+                @dispatcher.dispatch
+                sleep @dispatcher.task_delay
+            end
+        end
 
-		def self.log(level, text)
-			return unless @arguments.verbose if level == :verbose
-			return unless level == :error if @arguments.quiet
-			(level == :error ? $stderr : $stdout).puts text
-		end
-	end
+        def self.log(level, text)
+            return unless @arguments.verbose if level == :verbose
+            return unless level == :error if @arguments.quiet
+            (level == :error ? $stderr : $stdout).puts text
+        end
+    end
 
-	Core.new.start
+    Core.new.start
 end
